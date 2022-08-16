d5aabf94815d4fd17eca9719b5cafe3f05b6868f
PetrMakeev <109295765+PetrMakeev@users.noreply.github.com>
2022-08-16 23:19:19 +0500

    доделал логику обработки телеканалов

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ lst_txt = []
 divH = 2
 list_week = ['ПОНЕДЕЛЬНИК', 'ВТОРНИК', 'СРЕДА', 'ЧЕТВЕРГ', 'ПЯТНИЦА', 'СУББОТА', 'ВОСКРЕСЕНЬЕ']
 
+def set_scr():
+    #подготовка экрана
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Программа для обработки программы телеканалов ')
+    print('---------------------------------------------')
 
 
 # поправка на часовой пояс
@@ -70,6 +75,7 @@ def txt_to_list(path_prog):
     # выводим телеканаля и запрашиваем действия
     Loop_list_Ch = True
     while Loop_list_Ch:
+        set_scr()
         # выводим список на экран для проверки названий и порядка 
         print('----------------------------')
         print('  №  |  Название телеканала ')
@@ -95,15 +101,81 @@ def txt_to_list(path_prog):
             # Если полученный ввод не число, будет вызвано исключение
             except ValueError:
                 # Цикл будет повторяться до правильного ввода
-                print("Error! Это не число, попробуйте снова.")
+                print("Это не число, попробуйте снова.")
             else:
                 if mode_lst>-1 and mode_lst<3:
+                    # выход из цикла выбора действия
                     Loop_action_Ch = False
         
         # продолжаем обработку телеканалов
         if mode_lst == 0:  
             Loop_list_Ch = False
-        
+
+        # переименовка телеканала
+        if mode_lst == 1:
+            loop_sel_channel = True
+            while loop_sel_channel:
+                try:
+                    id_channel = int(input('Укажите номер телеканала, для переименования <1 - '+str(len(lst_Ch)) + '>: '))
+                # Если полученный ввод не число, будет вызвано исключение
+                except ValueError:
+                    # Цикл будет повторяться до правильного ввода
+                    print("Это не число, попробуйте снова.")
+                if id_channel>0 and id_channel<len(lst_Ch)+1: 
+                     loop_sel_channel = False
+                else:
+                    print('Вы указали неверный номер телеканала')
+            # указываем новое имя телеканала
+            loop_rename_chanel = True
+            while loop_rename_chanel:
+                rename_channel = (input('Укажите новое название телеканала <' + lst_Ch[id_channel-1][1] + '>: '))
+                if len(rename_channel)>0 : 
+                    loop_rename_chanel = False
+            print('Переименовываем телеканал')
+
+        # изменение порядка телеканалов
+        if mode_lst == 2:
+            loop_sel_id = True
+            while loop_sel_id:
+                try:
+                    id1_channel = int(input('Укажите номер телеканала, для перемещения в списке <1 - '+str(len(lst_Ch)) + ': '))
+                # Если полученный ввод не число, будет вызвано исключение
+                except ValueError:
+                    # Цикл будет повторяться до правильного ввода
+                    print("Это не число, попробуйте снова.")
+                if id1_channel>0 and id1_channel<len(lst_Ch)+1:
+                    loop_sel_id = False
+            loop_sel_pos = True
+            while loop_sel_pos:
+                try:
+                    id2_channel = int(input('Укажите на какое место переместить телеканал <' + lst_Ch[id1_channel-1][1] + '> в списке <1 - '+str(len(lst_Ch)) + ': '))
+                # Если полученный ввод не число, будет вызвано исключение
+                except ValueError:
+                    # Цикл будет повторяться до правильного ввода
+                    print("Это не число, попробуйте снова.")
+                if id2_channel>0 and id2_channel<len(lst_Ch)+1:
+                    loop_sel_pos = False
+            # если номера разные меняем места                    
+            if id1_channel!=id2_channel:
+                print('Меняем место')
+
+    # редактирование закончено сливаем откорректированный список телеканалов в Channel.txt
+    # сливаем список каналов в справочник каналов в Channel.txt
+    str_ch=''
+    for el in lst_Ch:
+        str_ch += ( el[0]+ '|' + el[1] + '\n')
+    try:
+        with open('Channel.txt', 'w') as file_w:
+            file_w.writelines(str_ch)
+    except:
+        print('Файл Channel.txt заблокирован для вывода списка каналов!')            
+            
+
+
+                    
+                
+
+                     
         
 
     
@@ -304,10 +376,7 @@ def txt_to_prog(path_prog):
 # основное тело программы
 def main():
 
-    #подготовка экрана
-    clear
-    print('Программа для обработки программы телеканалов ')
-    print('---------------------------------------------')
+    # def set_scrn()
 
     # определяем окружение
     path_prog = os.getcwd()
