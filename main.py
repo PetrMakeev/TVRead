080a2f284de6b2665774a2069672e5de9f54e1ff
PetrMakeev <p.makeev@gmail.com>
2022-08-17 17:44:34 +0500

    Сохранение программы в списки по дням недели

--- main.py
+++ main.py
@@ -22,12 +22,13 @@ def timeDiv(strTime):
     if strHourN>23 : strHourN = strHourN - 24
     if strHourN<0  : strHourN = 24 - strHourN 
     if strHourN<10:
-        Rezult = '0' + str(strHourN) + strTime[2:]
+        Rezult = '0' + str(strHourN) + '.' + strTime[2:]
     else:
-        Rezult = str(strHourN) + strTime[2:]
+        Rezult = str(strHourN) + '.' + strTime[2:]
     return Rezult
 
 
+# готовим список каналов
 def txt_to_list(path_prog):
     # создаем список файлов источников
     global lst_txt
@@ -174,85 +175,6 @@ def txt_to_list(path_prog):
             
 
 
-                    
-                
-
-                     
-        
-
-    
-
-    
-
-    
-
-
-
-
-def txt_to_Channel(path_prog):
-    # считываем txt в список
-    
-    # сканируем файлы txt и формируем списки каналов и программ
-    for name_files in lst_txt:
-        # открываем файлы txt построчно сохраняем в список
-        with open(name_files, 'r') as file_r:
-            str_txt = file_r.readlines()
-        
-        # обрабатываем список с программой из файла источника 
-        #for str_line in str_txt:
-            # если строка начинается с цифры то это программа, если с буквы то день недели
-        
-        
-        
-        
-        # for str_line in str_txt:
-        #     # делим строку на две части по пробелу
-        #     str_sub_line = str_line.rstrip().split(' ', 2)
-
-
-
-        #     # пропускаем пустые строки
-        #     if len(str_sub_line)>1:
-        #         str_sub_line1 = str_sub_line[0] 
-        #         str_sub_line2 = str_sub_line[1]
-                
-        #         # пропускаем прочерки
-        #         if str_sub_line2.find('---')<0 :         
-
-        #             # выбираем название канала 
-        #             if str_sub_line[0] == '':
-
-        #                 # пропускаем дни недели
-        #                 if not str_sub_line2.upper() in list_week :
-        #                     str_sub_lineCh = str_line.strip().split(' ')
-        #                     name_Ch = ''
-        #                     for str_ch in str_sub_lineCh:
-        #                         if not str_ch[0]=='!':
-        #                             name_Ch = name_Ch + ' ' + str_ch
-        #                     if name_Ch.strip() not in lst_Ch: lst_Ch.append(name_Ch.strip())
-        
-    # запрашиваем сортировку каналов
-    # print('Обработаны следующие телеканалы:\n')
-    # npp = 1
-    # for el in lst_Ch:
-    #     if npp>9:
-    #         print('< '+ str(npp) +' > - ' + el)
-    #     else:
-    #         print('<  '+ str(npp) +' > - ' + el)
-    #     npp += 1
-
-    # # сливаем перечень каналов в Channel.txt
-    # try:
-    #     with open('Channel.txt', 'w', encoding='utf-8') as file_w:
-    #         file_w.writelines(name_Ch)
-    # except:
-    #     print('Файл Channel.txt заблокирован для вывода списка каналов!')
-    
-    
-        
-
-
-
 def fill_Day():        
     # готовим заготовки списков программ по дням недели
     global lst_D1
@@ -265,115 +187,109 @@ def fill_Day():
 
     lst_D1 = [['~']]
     for el in lst_Ch:
-        lst_D1.append([el])
+        lst_D1.append([el[0]])
     lst_D1.remove(['~'])
     lst_D2 = [['~']]
     for el in lst_Ch:
-        lst_D2.append([el])
+        lst_D2.append([el[0]])
     lst_D2.remove(['~'])
     lst_D3 = [['~']]
     for el in lst_Ch:
-        lst_D3.append([el])
+        lst_D3.append([el[0]])
     lst_D3.remove(['~'])
     lst_D4 = [['~']]
     for el in lst_Ch:
-        lst_D4.append([el])
+        lst_D4.append([el[0]])
     lst_D4.remove(['~'])
     lst_D5 = [['~']]
     for el in lst_Ch:
-        lst_D5.append([el])
+        lst_D5.append([el[0]])
     lst_D5.remove(['~'])
     lst_D6 = [['~']]
     for el in lst_Ch:
-        lst_D6.append([el])
+        lst_D6.append([el[0]])
     lst_D6.remove(['~'])
     lst_D7 = [['~']]
     for el in lst_Ch:
-        lst_D7.append([el])
+        lst_D7.append([el[0]])
     lst_D7.remove(['~'])  
 
 
 
 def txt_to_prog(path_prog):
-    # считываем txt в список
-    
-    # сканируем файлы txt и формируем списки каналов и программ
+    # сканируем файлы txt и формируем  программы
     for name_files in lst_txt:
         # открываем файлы txt построчно сохраняем в список
-        with open(name_files, 'w', encoding='utf-8') as file_r:
+        with open(name_files, 'r') as file_r:
             str_txt = file_r.readlines()
-        
+            
         # устанавливаем первый день недели, канал, программу
-        name_Day = 'ПОНЕДЕЛЬНИК'
-        name_Ch = ''
+        # name_Day = 'ПОНЕДЕЛЬНИК'
+        name_Ch = os.path.basename(name_files)[0]
         name_Pr = ''
-     
+    
         # обрабатываем список с программой из файла источника 
         for str_line in str_txt:
-            # делим строку на две части по пробелу
-            str_sub_line = str_line.rstrip().split(' ', 2)
+            # проверяем первый знак 
+            str_tmp = str_line[:-1].strip()
+            # и пустые строки
+            if len(str_tmp)<3: continue
+ 
+            str_sub_lineD = str_tmp.split(',',2)
+            if str_sub_lineD[0].upper() in list_week:
+                # в строке день недели
+                name_Day = str_sub_lineD[0].upper()
 
-            # пропускаем пустые строки
-            if len(str_sub_line)>1:
-                str_sub_line1 = str_sub_line[0] 
-                str_sub_line2 = str_sub_line[1]
-                
-                # пропускаем прочерки
-                if str_sub_line2.find('---')<0 :         
+            else:
+                # в строке программа
+                # отделяем время программы от названия программы
+                if len(str_sub_lineD)>0:
+                    str_sub_lineD = str_tmp.split(' ',1)
 
-                    # выбираем название канала и дни недели
-                    if str_sub_line[0] == '':
-
-                        if str_sub_line2.upper() in list_week :
-                            name_Day = str_sub_line2.upper()
-                        else:
-                            str_sub_lineCh = str_line.strip().split(' ')
-                            name_Ch = ''
-                            for str_ch in str_sub_lineCh:
-                                if not str_ch[0]=='!':
-                                    name_Ch = name_Ch + ' ' + str_ch
-
-                            
-                    else:
-                        # отделяем время программы от названия программы
-                        str_sub_lineD = str_line.split(' ')
-                        name_Pr = ''
-                        # перебираем список времени начала программы, меняем согласно часовому поясу и склеиваем в строку обратно
-                        for str_time in str_sub_lineD:
-                            if not str_time[:2].isdigit():
-                                name_Pr = name_Pr + ' ' + str_time
-                            elif len(str_time)<5:
-                                name_Pr = name_Pr + ' ' + str_time
-                            elif not str_time[2]=='.':
-                                name_Pr = name_Pr + ' ' + str_time
-                            else:
-                                name_Pr = name_Pr + ' ' + timeDiv(str_time) 
-                                                          
-                        
-                        # собираем список [канал, [программа]] переработать!!!
-                        if str_sub_line1[0][:2].isdigit():
-
-                            if name_Day == 'ПОНЕДЕЛЬНИК':
-                                lst_D1[lst_Ch.index(name_Ch.strip())].append(name_Pr.strip())                            
-
-                            elif name_Day == 'ВТОРНИК':
-                                lst_D2[lst_Ch.index(name_Ch.strip())].append(name_Pr.strip())
-
-                            elif name_Day == 'СРЕДА':
-                                lst_D3[lst_Ch.index(name_Ch.strip())].append(name_Pr.strip())
-
-                            elif name_Day == 'ЧЕТВЕРГ':
-                                lst_D4[lst_Ch.index(name_Ch.strip())].append(name_Pr.strip())
-
-                            elif name_Day == 'ПЯТНИЦА':
-                                lst_D5[lst_Ch.index(name_Ch.strip())].append(name_Pr.strip())
-
-                            elif name_Day == 'СУБЮОТА':
-                                lst_D6[lst_Ch.index(name_Ch.strip())].append(name_Pr.strip())
-
-                            if name_Day == 'ВОСКРЕСЕНЬЕ':
-                                lst_D7[lst_Ch.index(name_Ch.strip())].append(name_Pr.strip())
+                    name_Pr = timeDiv(str_sub_lineD[0]) + '|' + str_sub_lineD[1] 
     
+                    # собираем список [канал, [программа]] 
+                    if name_Day == 'ПОНЕДЕЛЬНИК':
+                        for i, el in enumerate(lst_Ch):
+                            if el[0]==name_Ch:
+                                lst_D1[i].append(name_Pr.strip())                            
+
+                    elif name_Day == 'ВТОРНИК':
+                        for i, el in enumerate(lst_Ch):
+                            if el[0]==name_Ch:
+                                lst_D2[i].append(name_Pr.strip())
+
+                    elif name_Day == 'СРЕДА':
+                        for i, el in enumerate(lst_Ch):
+                            if el[0]==name_Ch:
+                                lst_D3[i].append(name_Pr.strip())
+
+                    elif name_Day == 'ЧЕТВЕРГ':
+                        for i, el in enumerate(lst_Ch):
+                            if el[0]==name_Ch:
+                                lst_D4[i].append(name_Pr.strip())
+
+                    elif name_Day == 'ПЯТНИЦА':
+                        for i, el in enumerate(lst_Ch):
+                            if el[0]==name_Ch:
+                                lst_D5[i].append(name_Pr.strip())
+
+                    elif name_Day == 'СУББОТА':
+                        for i, el in enumerate(lst_Ch):
+                            if el[0]==name_Ch:
+                                lst_D6[i].append(name_Pr.strip())
+
+                    elif name_Day == 'ВОСКРЕСЕНЬЕ':
+                        for i, el in enumerate(lst_Ch):
+                            if el[0]==name_Ch:
+                                lst_D7[i].append(name_Pr.strip())
+
+
+ 
+ 
+ 
+ 
+       
 
 # основное тело программы
 def main():
@@ -386,23 +302,18 @@ def main():
    # заполняем список сканируемых файлов каналов
     txt_to_list(path_prog)
 
+    # создаем заготовки списков программ по дням
+    fill_Day()
+
     # считываем каналы 
-    txt_to_Channel(path_prog)
+    txt_to_prog(path_prog)
 
 
  
     # сделать запрос поправки на часовой пояс, по умолчанию поставить 2 часа
     #read_divH()
 
-
-
-    # создаем заготовки списков программ по дням
-    # fill_Day()
-
-    # считываем программы 
-    # txt_to_prog(path_prog)
- 
-   
+    print('w')   
 
 
 # точка входа.
